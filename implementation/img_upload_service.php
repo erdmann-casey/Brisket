83b945cf862bef2de4ed5642c7a7b8d23e2bcf5a
Injection <casey@injecti0n.org>
2017-11-12 20:30:36 -0500

    Finished core front end implementation

--- implementation/img_upload_service.php
+++ implementation/img_upload_service.php
@@ -1,3 +1,32 @@
+<html lang="en">
+
+<head>
+    <title>Brisket</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-dark justify-content-between">
+        <div class="container">
+            <form class="form-inline">
+                <a class="navbar-brand" href="index.html">Brisket</a>
+                <button class="btn btn-primary btn-sm" type="button" style="margin-right:16px">+New Paste</button>
+                <button class="btn btn-success btn-sm" type="button">+New Image</button>
+            </form>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search..</button>
+            </form>
+        </div>
+    </nav>
+
+    <hr>
+
 <?php
 
 require_once 'services_handler.php';
@@ -44,11 +73,20 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
         $services->uploadImage($file_rename);
         $generateImageLink = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]/uploads/" . $file_rename;
-        echo "Image URL: <br/>" . '<a href="' . $generateImageLink . '">' . $generateImageLink . '</a>';
+        echo "<div class='col-md-12'><h3>Image URL: </h3><br/>" . '<img class="img-responsive" src="'. $generateImageLink .'"/><br/><a href="' . $generateImageLink . '">' . $generateImageLink . '</a></div>';
     } else {
         echo "An issue or error occurred, your file was not uploaded.";
     }
 }
 ?>
+
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</body>
+
+</html>
 
 
--- implementation/text_upload_service.php
+++ implementation/text_upload_service.php
@@ -1,3 +1,32 @@
+<html lang="en">
+
+<head>
+    <title>Brisket</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-dark justify-content-between">
+        <div class="container">
+            <form class="form-inline">
+                <a class="navbar-brand" href="index.html">Brisket</a>
+                <button class="btn btn-primary btn-sm" type="button" style="margin-right:16px">+New Paste</button>
+                <button class="btn btn-success btn-sm" type="button">+New Image</button>
+            </form>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search..</button>
+            </form>
+        </div>
+    </nav>
+
+    <hr>
+
 <?php
 
 require_once 'services_handler.php';
@@ -23,6 +52,11 @@ if (strlen($uploadText) >= 500) {
     $uploadOk = 0;
 }
 
+if(strlen($uploadText) == 0){
+    echo "Posts can not be empty<br/>";
+    $uploadOk = 0;
+}
+
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "An issue or error occurred, your text was not posted.<br/>";
@@ -30,6 +64,16 @@ if ($uploadOk == 0) {
 } else {
         $textID = $services->uploadText($uploadText);
         $generateTextLink = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]/text_fetch_service.php?id=" . $textID;
-        echo "Text URL: <br/>" . '<a href="' . $generateTextLink . '">' . $generateTextLink . '</a>';
+        echo "<div class='col-md-12'><h3>Text URL: </h3><br/>" . '<div class="col-md-8"><textarea readonly rows="10" class="form-control">'. $uploadText .'</textarea><a href="' . $generateTextLink . '">' . $generateTextLink . '</a></div></div>';
 }
 ?>
+
+
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</body>
+
+</html>
